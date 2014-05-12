eabf55123bf261d9a7676c8d358f3fb371a944e3
marco <marsucsb@gmail.com>
2014-05-11 17:05:52 -0700

    Specify AndroidManifest.xml location to run in IDE.

--- src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
+++ src/test/java/com/example/activity/DeckardActivityRobolectricTest.java
@@ -6,10 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
-import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
+@Config(manifest = "./src/main/AndroidManifest.xml")
 @RunWith(RobolectricTestRunner.class)
 public class DeckardActivityRobolectricTest {
 
